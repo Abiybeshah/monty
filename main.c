e11c2b7be5b17b30cc8b0b82e53ec5e3bfd129cd
Abiybeshah <abiyjem99@gmail.com>
2024-03-29 01:59:41 +0300

    stack

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "monty.h"
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interprter
 * @argc: num of arguments
@@ -14,7 +14,6 @@ int main(int argc, char *argv[])
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
-	bus_t bus = {NULL, NULL, NULL, 0};
 
 	if (argc != 2)
 	{
